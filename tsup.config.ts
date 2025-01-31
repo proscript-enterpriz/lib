bfb0b33724efca7f86d28f39c89db531af9f4c94
dashka0212 <44989745+dashka0212@users.noreply.github.com>
2025-01-31 19:54:21 +0800

    dependencies changed

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	format: ['cjs', 'esm'],
-	entry: ['./src/index.ts'],
-	dts: true,
-	shims: true,
-	skipNodeModulesBundle: true,
-	clean: true,
-	minify: true,
-	minifyWhitespace: true,
-	minifyIdentifiers: true,
-	minifySyntax: true,
-	treeshake: true,
+  format: ['cjs', 'esm'],
+  entry: ['./src/index.ts'],
+  dts: true,
+  shims: true,
+  skipNodeModulesBundle: true,
+  clean: true,
+  minify: true,
+  minifyWhitespace: true,
+  minifyIdentifiers: true,
+  minifySyntax: true,
+  treeshake: true,
 });
